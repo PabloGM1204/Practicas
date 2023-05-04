0a9137bffd906d48eaca940fdcc98cdb1f546822
PabloGM1204 <115071457+PabloGM1204@users.noreply.github.com>
2023-05-04 16:34:01 +0200

    CRUD realizado con Spring Boot

---
+++ ProyectoFacturaSpring/src/main/java/com/proyectofactura/proyectofacturaspring/ProyectoFacturaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofactura.proyectofacturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFacturaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFacturaSpringApplication.class, args);
+    }
+
+}
---
+++ ProyectoFacturaSpring/src/test/java/com/proyectofactura/proyectofacturaspring/ProyectoFacturaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofactura.proyectofacturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFacturaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
